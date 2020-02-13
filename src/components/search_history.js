6bbe5a9d2702853b9e37e18741be4476f457bdf7
Zeke Lin <zeke5252@yahoo.com.tw>
2020-02-13 15:56:24 +0800

    Sass for word component done. Display all search words done

--- src/components/search_history.js
+++ src/components/search_history.js
@@ -1,84 +1,75 @@
 import React from "react";
 import styles from "../sass/main.scss";
 import { connect } from "react-redux";
-import Word_real from './word'
+import Word_real from "./word";
 
-class Word extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={styles.s_container}>
-        <div className={styles.s_times}>45</div>
-        <div className={styles.s_word}>patient</div>
-        <div className={styles.s_phonetic}>/ˈpeɪʃ(ə)nt/</div>
-        <div className={styles.s_origin}>
-          Middle English from Old French, from Latin patient- ‘suffering’, from
-          the verb pati.
-        </div>
-        <div>
-          <div className={styles.s_meaning}>
-            adjective
-            <div className={styles.s_block}>
-              <div className={styles.s_definition}>
-                Able to accept or tolerate delays, problems, or suffering
-                without becoming annoyed or anxious.
-              </div>
-              <div className={styles.s_example}>
-                be patient, your time will come
-              </div>
-              <div className={styles.s_synonyms}>
-                forbearing, uncomplaining, tolerant, long-suffering, resigned,
-                stoical
-              </div>
-            </div>
-          </div>
-          <div className={styles.s_meaning}>
-            none
-            <div className={styles.s_block}>
-              <div className={styles.s_definition}>
-                A person receiving or registered to receive medical treatment.
-              </div>
-              <div className={styles.s_example}>
-                many patients in the hospital were more ill than she was
-              </div>
-              <div className={styles.s_synonyms}>
-                sick person, case, sufferer, victim
-              </div>
-            </div>
-            <div className={styles.s_block}>
-              <div className={styles.s_definition}>
-                The semantic role of a noun phrase denoting something that is
-                affected or acted upon by the action of a verb.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// Import Word class. (done)
+// Get data from firestore. (done)
+// Compare style object in search_history with that in popup_search. (done)
+// Syncronize the amount of classes. (done)
+// Prepare a style object, pass to word component. (done)
+// Use {expressions} and array to render components.
+// Add remove word function
+// Select By time to re-sort results
+// Add my favorite function
+// Review inifinite scroll.
 
 class Search_history extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      allWords: [],
+      styleParent: {
+        s_container: styles.s_container,
+        s_times: styles.s_times,
+        s_word: styles.s_word,
+        s_phonetic: styles.s_phonetic,
+        s_origin: styles.s_origin,
+        s_meaning: styles.s_meaning,
+        s_block: styles.s_block,
+        s_definition: styles.s_definition,
+        s_example: styles.s_example,
+        s_synonyms: styles.s_synonyms,
+        s_speech: styles.s_speech
+      }
+    };
   }
 
-  
   componentDidMount() {
     let uid = this.props.userUID;
     let db = firebase.firestore();
-    console.log('uid=', uid)
+    let tempWordArr = [];
+    console.log("uid=", uid);
     db.collection("users")
       .doc(`${uid}`)
       .collection("Search_history")
-      .doc("period")
       .get()
-      .then(res => console.log(res.data()))
+      .then(res =>
+        res.forEach(eachWord => {
+          tempWordArr.push(eachWord.data());
+          this.setState({
+            allWords: tempWordArr
+          });
+        })
+      )
       .catch(function(error) {
         console.log("Error getting document:", error);
       });
+
+    // db.collection("users")
+    //   .doc(`${uid}`)
+    //   .collection("Search_history")
+    //   .doc("Sunday")
+    //   .get()
+    //   .then(word => {
+    //     console.log(word.id, "=", word.data());
+    //     this.setState({
+    //       resDetails: word.data()
+    //     });
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Error getting document:", error);
+    //   });
   }
 
   render() {
@@ -109,9 +100,16 @@ class Search_history extends React.Component {
           <span> Favorite </span>
         </div>
         <ul>
-          <li>
-            <Word />
-          </li>
+          {this.state.allWords.map((word, i) => {
+            return (
+              <li key={i}>
+                <Word_real
+                  resDetails={word}
+                  styleParent={this.state.styleParent}
+                />
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
@@ -125,7 +123,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Search_history);
-
-
-
-// 觀察firestore上查過的字的結構是否等同於直接收到的response
--- src/components/word.js
+++ src/components/word.js
@@ -4,70 +4,118 @@ import styles from "../sass/main.scss";
 import {} from "../actions/";
 
 // For search history only first
-// Use props or state from firestore as an parameter of showPopup function to return an jsx snippet 
+// Use props or state from firestore as an parameter of showPopup function to return an jsx snippet
+// Make sure the catogory is in a right sorting method when the word was saved to firestore
 
 class Word extends React.Component {
   constructor(props) {
     super(props);
-    this.showPopup = this.showPopup.bind(this);
-    this.showAllContent = this.showAllContent.bind(this);
   }
 
-  showPopup(showContent) {
-    if (showContent === "word") {
-      // declare all
-      let data = this.props.resDetails;
-      // Full content
-      let renderFull = Object.keys(data).map((catogory, index1) => (
-        <div key={index1}>
-          <div className={styles.s_word}>{catogory}</div>
-          <div>
-            {typeof Object.values(data)[index1] === "string" ? (
-              Object.values(data)[index1]
-            ) : (
-              <div>{Object.keys(data.meaning).map((catogory, index) => {
+  showPopup() {
+    // Reverse the keys of data on Firestore to make the highlighted word shown at the top
+    let oldData = this.props.resDetails;
+    let data = {};
+    let keys = [];
+    for (let key in oldData) {
+      keys.push(key);
+    }
+    for (let i = keys.length - 1; i >= 0; i--) {
+      let value = oldData[keys[i]];
+      data[keys[i]] = value;
+    }
+
+    let renderFull = Object.keys(data).map((catogory, index1) => (
+      <div key={index1}>
+        <div className={this.props.styleParent.s_times}>45</div>
+        {/* word, phonetic, origin, meaning */}
+        {/* <div >{catogory}</div> */}
+        <div
+          className={
+            /\s/.test(Object.values(data)[index1])
+              ? ""
+              : Object.values(data)[index1].includes("/")
+              ? this.props.styleParent.s_phonetic
+              : this.props.styleParent.s_word
+          }
+        >
+          {typeof Object.values(data)[index1] === "string" ? (
+            // patient, kk pronounciation, middleEnglish from
+            Object.values(data)[index1]
+          ) : (
+            <div>
+              {Object.keys(data.meaning).map((catogory, index) => {
                 return (
                   <div key={index}>
-                    <span className={styles.s_speech}>{catogory}</span>
+                    {/* adjective, noun */}
+                    <span className={this.props.styleParent.s_speech}>
+                      {catogory}
+                    </span>
                     {data.meaning[catogory].map((detailObj, i) => {
                       return (
                         <div key={i}>
-                          .{Object.keys(detailObj).map((eachDetailObjName, i) => {
-                            return <div key={i}>{eachDetailObjName}
-                            <div>{typeof Object.values(detailObj)[i] === "string" ? Object.values(detailObj)[i] : Object.values(detailObj)[i].map((synonym,i)=>{
-                              return <span key={i}>{synonym}</span>
-                            })}</div>
-                            </div>;
-                          })}
+                          {Object.keys(detailObj).map(
+                            (eachDetailObjName, i) => {
+                              return (
+                                <div key={i}>
+                                  {/* definition, example, synonyms */}
+                                  <div
+                                    className={this.props.styleParent.s_block}
+                                  >
+                                    {eachDetailObjName}
+                                  </div>
+                                  <div>
+                                    {typeof Object.values(detailObj)[i] ===
+                                    "string"
+                                      ? // Able to accept..., be patient....
+                                        Object.values(detailObj)[i]
+                                      : Object.values(detailObj)[i].map(
+                                          (synonym, i) => {
+                                            return (
+                                              // forbearing, uncomplaining,...
+                                              <span
+                                                key={i}
+                                                className={
+                                                  this.props.styleParent
+                                                    .s_synonyms
+                                                }
+                                              >
+                                                {synonym},{" "}
+                                              </span>
+                                            );
+                                          }
+                                        )}
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
                         </div>
                       );
                     })}
                   </div>
                 );
-              })}</div>
-            )}
-          </div>
+              })}
+            </div>
+          )}
         </div>
-      ));
+      </div>
+    ));
 
-      return (
-        <div
-          className={styles.popupSearchContainer}
-        >
-          {this.props.searchContent === "partial" ? renderPartial : renderFull}
-        </div>
-      );
-    }
+    // Use props as a source of css style
+    return (
+      <div className={this.props.styleParent.s_container}>{renderFull}</div>
+    );
   }
 
   render() {
-    return this.showPopup(this.props.showContent);
+    return this.showPopup();
   }
 }
 
 function mapStateToProps(state) {
   return {
-    userUID: state.userUID
+    state: state
   };
 }
 
